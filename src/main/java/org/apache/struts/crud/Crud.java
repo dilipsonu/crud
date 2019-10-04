75bce338d6af901297b6221ab11d0553d990a3ac
Dilip Kumar <>
2019-10-04 16:17:12 -0500

    added jsp

---
+++ src/main/java/org/apache/struts/crud/Crud.java
@@ -0,0 +1,29 @@
+package org.apache.struts.crud;
+
+import org.apache.struts2.dispatcher.filter.StrutsPrepareAndExecuteFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@SpringBootApplication
+@EnableWebSecurity
+public class Crud {
+
+    public static void main (String [] args)
+    {
+        SpringApplication.run(Crud.class,args);
+    }
+
+
+    @Bean
+    public FilterRegistrationBean struts2()
+    {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new StrutsPrepareAndExecuteFilter());
+        filterRegistrationBean.addUrlPatterns("*.action");
+
+        return filterRegistrationBean;
+    }
+}
